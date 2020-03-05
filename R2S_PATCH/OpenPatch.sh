329a6b87efb5a038920ada1123f0135257300d7d
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-03-05 21:45:39 +0800

    Update OpenPatch.sh

--- R2S_PATCH/OpenPatch.sh
+++ R2S_PATCH/OpenPatch.sh
@@ -41,6 +41,7 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin pack
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/srelay package/lean/srelay
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks package/lean/microsocks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks package/lean/dns2socks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package/lean/redsocks2
 sed -i 's,$(STAGING,#$(STAGING,g' package/lean/v2ray-plugin/Makefile
 #订阅转换
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/subconverter package/new/subconverter
