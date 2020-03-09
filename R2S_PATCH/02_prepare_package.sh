616c24003821b4d75eb6e4304bc932a412361c69
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-03-09 10:23:58 +0800

    Update 02_prepare_package.sh

--- R2S_PATCH/02_prepare_package.sh
+++ R2S_PATCH/02_prepare_package.sh
@@ -7,6 +7,7 @@ svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/system/
 #网易云解锁
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusicGo package/lean/UnblockNeteaseMusicGo
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-unblockmusic package/lean/luci-app-unblockmusic
+sed -i "s,+UnblockNeteaseMusic_NodeJS:UnblockNeteaseMusic ,,g" package/lean/luci-app-unblockmusic/Makefile
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-lib-fs package/lean/luci-lib-fs
 #irqbalance
 sed -i 's/0/1/g' feeds/packages/utils/irqbalance/files/irqbalance.config
