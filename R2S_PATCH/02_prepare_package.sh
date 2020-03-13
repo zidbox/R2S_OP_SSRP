c464b77c99656f4ba0ce91aa35fc47b56124821f
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-03-14 00:39:54 +0800

    Update 02_prepare_package.sh

--- R2S_PATCH/02_prepare_package.sh
+++ R2S_PATCH/02_prepare_package.sh
@@ -87,4 +87,6 @@ chmod -R 755 ./
 #生成默认配置及缓存
 rm -rf .config
 mv R2.config .config
+#修正架构
+sed -i "s,boardinfo.system,'ARMv8',g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 exit 0
