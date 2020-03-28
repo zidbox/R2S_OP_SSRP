ec0d83e25c2d095a6bb83020c0688f404fa06fd8
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-03-28 22:24:53 +0800

    Update 01_prepare_mixedwrt.sh

--- 01_prepare_mixedwrt.sh
+++ 01_prepare_mixedwrt.sh
@@ -5,8 +5,6 @@ cp -r ./SEED/. ./
 cp -r ./OTHER/. ./
 git clone -b master https://github.com/QiuSimons/R2S_PATCH.git ./R2S_TMP
 cp -r ./R2S_TMP/. ./R2S_PATCH
-
-
 mkdir friendlywrt-rk3328
 cd friendlywrt-rk3328
 repo init -u https://github.com/friendlyarm/friendlywrt_manifests -b master-v19.07.1 -m rk3328.xml --repo-url=https://github.com/friendlyarm/repo --no-clone-bundle --depth=1
@@ -22,8 +20,6 @@ tar zxvf ${latest_release}  --strip-components 1 -C ./friendlywrt-rk3328/friendl
 rm -f ./friendlywrt-rk3328/friendlywrt/feeds.conf.default
 wget -P friendlywrt-rk3328/friendlywrt/ https://raw.githubusercontent.com/openwrt/openwrt/openwrt-19.07/feeds.conf.default
 rm -f ${latest_release}
-
-
 rm -rf ./R2S_PATCH/.git
 cp -rf ./R2S_PATCH/. ./friendlywrt-rk3328/friendlywrt/
 cp -f ./R2S_PATCH/05_patch_kernel.sh ./friendlywrt-rk3328/05_patch_kernel.sh
