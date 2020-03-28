49455505ee137d03ba8ee21630307e073aa2d679
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-03-28 11:03:00 +0800

    Update fullcone.sh

--- FULLCONE/fullcone.sh
+++ FULLCONE/fullcone.sh
@@ -3,6 +3,5 @@
 cd kernel
 patch -Ntp1 < ../../fullcone.patch
 cd ..
-sed -i '/CONFIG_NETFILTER_XT_TARGET_HMARK/i\CONFIG_NETFILTER_XT_TARGET_FULLCONENAT=y' kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
-sed -i '/CONFIG_IP_NF_TARGET_NETMAP/i\CONFIG_IP_NF_TARGET_FULLCONENAT=y' kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
+sed -i '/CONFIG_NETFILTER_XT_TARGET_HMARK/i\CONFIG_NETFILTER_XT_TARGET_FULLCONENAT=m' kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
 exit 0
