ef63dbc7effeaae5a2d8fc3642a4f4ea2e499cbc
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-03-31 00:00:23 +0800

    Update 02_prepare_package.sh

--- R2S_PATCH/02_prepare_package.sh
+++ R2S_PATCH/02_prepare_package.sh
@@ -99,5 +99,5 @@ rm -rf .config
 #修正架构
 sed -i "s,boardinfo.system,'ARMv8',g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 #mv ../../scripts/check_wan4.sh package/base-files/files/usr/bin && sed -i '/exit/i\/bin/sh /usr/bin/check_wan4.sh &' package/base-files/files/etc/rc.local
-#mv ../../scripts/fullcone.sh package/base-files/files/usr/bin && sed -i '/exit/i\/bin/sh /usr/bin/fullcone.sh &' package/base-files/files/etc/rc.local
+mv ../../scripts/fullcone.sh package/base-files/files/usr/bin && sed -i '/exit/i\/bin/sh /usr/bin/fullcone.sh &' package/base-files/files/etc/rc.local
 exit 0
