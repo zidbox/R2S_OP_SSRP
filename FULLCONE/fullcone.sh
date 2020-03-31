f4621929fc5b6da0da63fd1d57404e81b4f6c490
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-03-31 22:33:47 +0800

    Update fullcone.sh

--- FULLCONE/fullcone.sh
+++ FULLCONE/fullcone.sh
@@ -3,6 +3,7 @@
 cd kernel
 patch -Ntp1 < ../../fullcone.patch
 patch -Ntp1 < ../../952-net-conntrack-events-support-multiple-registrant.patch
+patch -Ntp1 < ../../000-printk.patch
 cd ..
 sed -i '/CONFIG_BLK_DEV_IO_TRACE/a\CONFIG_NETFILTER_XT_TARGET_FULLCONENAT=m' kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
 exit 0
