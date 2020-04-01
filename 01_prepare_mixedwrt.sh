f6ad976c2fa4b5a96155938259fffe4fbdd9b24c
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-04-01 21:43:27 +0800

    Update 01_prepare_mixedwrt.sh

--- 01_prepare_mixedwrt.sh
+++ 01_prepare_mixedwrt.sh
@@ -11,6 +11,7 @@ repo init -u https://github.com/friendlyarm/friendlywrt_manifests -b master-v19.
 repo sync -c --no-tags --no-clone-bundle -j8
 latest_feed="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+.tar.gz" |sed -n 1p |sed 's/v//g' |sed 's/.tar.gz//g')"
 sed -i 's,19.07.1,'"${latest_feed}"',g' device/friendlyelec/rk3328/common-files/etc/opkg/distfeeds.conf
+sed -i 's,ACCEPT,REJECT,g' device/friendlyelec/rk3328/default-settings/install.sh
 cd friendlywrt
 shopt -s extglob 
 rm --recursive --force !(dl)
