e59aefde4c4a4def6be24317e3ebd15e86a4d629
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-04-08 13:22:50 +0800

    Update 02_prepare_package.sh

--- R2S_PATCH/02_prepare_package.sh
+++ R2S_PATCH/02_prepare_package.sh
@@ -47,9 +47,9 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping package/lean/tcpping
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/proxychains-ng package/lean/proxychains-ng
 #订阅转换
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/subconverter package/new/subconverter
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/jpcre2 package/new/jpcre2
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/rapidjson package/new/rapidjson
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ctcgfw/subconverter package/new/subconverter
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ctcgfw/jpcre2 package/new/jpcre2
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ctcgfw/rapidjson package/new/rapidjson
 #清理内存
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ramfree package/lean/luci-app-ramfree
 #打印机
