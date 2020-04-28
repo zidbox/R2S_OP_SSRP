d6146fe5527c5f15d88dc6619c3fd5750b820f8a
QiuSimons <QiuSimons@users.noreply.github.com>
2020-04-28 19:54:57 +0800

    revert

--- 01_prepare_mixedwrt.sh
+++ 01_prepare_mixedwrt.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 clear
-#get env ready
 cp -r ./FULLCONE/. ./
 cp -r ./SEED/. ./
 cp -r ./OTHER/. ./
 git clone -b master https://github.com/QiuSimons/R2S_PATCH.git ./R2S_TMP
-##cp -r ./R2S_TMP/. ./R2S_PATCH
-#pull friendlywrt
+#cp -r ./R2S_TMP/. ./R2S_PATCH
 mkdir friendlywrt-rk3328
 cd friendlywrt-rk3328
 repo init -u https://github.com/friendlyarm/friendlywrt_manifests -b master-v19.07.1 -m rk3328.xml --repo-url=https://github.com/friendlyarm/repo --no-clone-bundle --depth=1
 repo sync -c --no-tags --no-clone-bundle -j8
 cd friendlywrt/ && git fetch --unshallow
 cd ..
-cd kernel/ && git fetch --unshallow
-cd ..
-#modify friendlywrt
 latest_feed="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+.tar.gz" |sed -n 1p |sed 's/v//g' |sed 's/.tar.gz//g')"
 sed -i 's,19.07.1,'"${latest_feed}"',g' device/friendlyelec/rk3328/common-files/etc/opkg/distfeeds.conf
 sed -i 's,ACCEPT,REJECT,g' device/friendlyelec/rk3328/default-settings/install.sh
-##cd friendlywrt
-##shopt -s extglob 
-##rm --recursive --force !(dl)
-##rm .*
-##cd ..
-##cd ..
-#pull openwrt
+#cd friendlywrt
+#shopt -s extglob 
+#rm --recursive --force !(dl)
+#rm .*
+#cd ..
+#cd ..
 mkdir opofficial
 latest_release="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+.tar.gz" |sed -n 1p)"
 curl -LO "https://github.com/openwrt/openwrt/archive/${latest_release}"
 tar zxvf ${latest_release}  --strip-components 1 -C ./opofficial
-##rm -f ./friendlywrt-rk3328/friendlywrt/feeds.conf.default
-##wget -P friendlywrt-rk3328/friendlywrt/ https://raw.githubusercontent.com/openwrt/openwrt/openwrt-19.07/feeds.conf.default
-##rm -f ${latest_release}
-#rebase friendlywrt
+#rm -f ./friendlywrt-rk3328/friendlywrt/feeds.conf.default
+#wget -P friendlywrt-rk3328/friendlywrt/ https://raw.githubusercontent.com/openwrt/openwrt/openwrt-19.07/feeds.conf.default
+#rm -f ${latest_release}
+######
 cd friendlywrt
 git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
 git remote add upstream https://github.com/openwrt/openwrt.git && git fetch upstream
@@ -51,7 +45,7 @@ cp -f ./opofficial/package/base-files/image-config.in ./friendlywrt/package/base
 cp -f ./opofficial/package/base-files/files/etc/banner ./friendlywrt/package/base-files/files/etc/banner
 cp -f ./opofficial/package/base-files/files/bin/config_generate ./friendlywrt/package/base-files/files/bin/config_generate
 cd ..
-#some cleanup
+######
 rm -rf ./R2S_PATCH/.git
 cp -rf ./R2S_PATCH/. ./friendlywrt-rk3328/friendlywrt/
 cp -f ./R2S_PATCH/05_patch_kernel.sh ./friendlywrt-rk3328/05_patch_kernel.sh
--- R2S_PATCH/05_patch_kernel.sh
+++ R2S_PATCH/05_patch_kernel.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-#enable 1512mhz
 cd kernel
 wget https://github.com/armbian/build/raw/master/patch/kernel/rockchip64-dev/RK3328-enable-1512mhz-opp.patch
 git apply RK3328-enable-1512mhz-opp.patch
-#rebase kernel
-git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
-git remote add upstream https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git && git fetch upstream
-git rebase 400467c^ --onto upstream/linux-5.4.y -X theirs
-rm -f ./arch/arm64/configs/nanopi-r2_linux_defconfig
-wget -O arch/arm64/configs/nanopi-r2_linux_defconfig https://raw.githubusercontent.com/fanck0605/friendlywrt-kernel/nanopi-r2-v5.4.y/arch/arm64/configs/nanopi-r2_linux_defconfig
-#apply openwrt patch
 cd ../
 git clone https://git.openwrt.org/openwrt/openwrt.git --depth=1
 cd openwrt
@@ -19,7 +11,6 @@ cd openwrt
 ./scripts/patch-kernel.sh ../kernel ./target/linux/octeontx/patches-5.4
 cp -a ./target/linux/generic/files/* ../kernel/
 cd ../
-#fullcone patch
 cd kernel/
 wget -O net/netfilter/xt_FULLCONENAT.c https://raw.githubusercontent.com/Chion82/netfilter-full-cone-nat/master/xt_FULLCONENAT.c
 git apply ../../FULLCONE/001-kernel-add-full_cone_nat.patch
