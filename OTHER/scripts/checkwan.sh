0731735d726b0323212c01a6d6fad7ac8cb2e2df
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-05-08 14:31:04 +0800

    Update checkwan.sh

--- OTHER/scripts/checkwan.sh
+++ OTHER/scripts/checkwan.sh
@@ -1,30 +1,63 @@
 #!/bin/sh
-#脚本放/bin目录，再用命令运行nohup sh /bin/checkwan.sh  1>/dev/null 2>&1 &
+# Copyright (c) 2020, Chuck <fanck0605@qq.com>
+#
+# this script is writing for openwrt
+# this script need the interface named 'lan'
+# to use this script, you must install 'jq' first
+# we need the `jq` to parse `ifstatus`'s result
 
-echo '脚本检测开始'
-tries=0
-wan=`ifconfig |grep inet| sed -n '1p'|awk '{print $2}'|awk -F ':' '{print $2}'`
-while true
-do
-# do something
-if ping -w 1 -c 1 119.29.29.29; then #ping dns通则
-	echo '网络正常'
-	tries=0
-else
-	sleep 1
-	if ping -w 1 -c 1 $wan; then #若ping网关通则
-		echo '网关正常'
-		tries=$((tries+1))
-	else #ping不通，重启防火墙
-		echo '网络崩溃重启防火墙'
-		/etc/init.d/firewall reload
-		sleep 10
-	fi
-	if [ $tries -ge 5 ]; then #连续ping dns 5次失败，重启wan口
-		tries=0
-		/etc/init.d/network restart
-		sleep 5
-	fi
+# usage: `/bin/sh /path/to/check_net4.sh >/dev/null 2>&1 &`
+
+logger 'Check Net4: Script started!'
+
+get_ipv4_address() {
+  if ! if_status=$(ifstatus $1); then
+    return 1
+  fi
+  echo $if_status | jq -r '."ipv4-address"[0]."address"'
+}
+
+if ! lan_addr=$(get_ipv4_address lan); then
+  logger "Check Net4: Don't support your network environment!"
+  exit 1
 fi
-sleep 2
+
+fail_count=0
+
+while :; do
+  sleep 2s
+
+  # try to connect
+  if ping -W 1 -c 1 "$lan_addr" >/dev/null 2>&1; then
+    # No problem!
+    if [ $fail_count -gt 0 ]; then
+      logger 'Check Net4: Network problems solved!'
+    fi
+    fail_count=0
+  else
+    # May have some problem
+    logger "Check Net4: Network may have some problems!"
+    fail_count=$((fail_count + 1))
+  fi
+
+  if [ $fail_count -ge 3 ]; then
+    # Must have some problem! We refresh the ip address and try again!
+    lan_addr=$(get_ipv4_address lan)
+
+    if ping -W 1 -c 1 "$lan_addr" >/dev/null 2>&1; then
+      continue
+    fi
+
+    logger 'Check Net4: Network problem! Firewall reloading...'
+    /etc/init.d/firewall reload >/dev/null 2>&1
+    sleep 2s
+
+    if ping -W 1 -c 1 "$lan_addr" >/dev/null 2>&1; then
+      continue
+    fi
+
+    logger 'Check Net4: Network problem! Network reloading...'
+    /etc/init.d/network reload >/dev/null 2>&1
+    sleep 2s
+  fi
 done
